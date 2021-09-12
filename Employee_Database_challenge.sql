7b3f38ef50912c1a6a381a760dbb033af33862b8
winneylee <81284888+winneylee@users.noreply.github.com>
2021-09-11 17:52:49 -0700

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -27,3 +27,15 @@ INTO retiring_titles
 from unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+
+--Create table for mentorship elifibility--
+SELECT DISTINCT ON (e.emp_no) e.emp_no,e.first_name,e.last_name,e.birth_date,de.from_date,de.to_date,ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
