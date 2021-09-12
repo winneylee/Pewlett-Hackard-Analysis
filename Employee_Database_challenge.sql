545315203fd16a8833f3074c3bb900e8e0c1600b
winneylee <81284888+winneylee@users.noreply.github.com>
2021-09-11 17:30:59 -0700

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,1 +1,29 @@
+-- Joining employees and titles tables
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
 
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+--Count # of employee in titles
+SELECT COUNT (ut.emp_no),ut.title
+INTO retiring_titles
+from unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
